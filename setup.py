9e941f59d127d08d4774c25c863c8ffbd4625132
Alvin Wan <hi@alvinwan.com>
2016-05-19 01:38:26 -0700

    updated setup.py with test command, pytest.ini

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-# class PyTest(TestCommand):
-#
-#     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-#
-#     def initialize_options(self):
-#         TestCommand.initialize_options(self)
-#         self.pytest_args = ['tests']
-#
-#     def finalize_options(self):
-#         TestCommand.finalize_options(self)
-#
-#     def run_tests(self):
-#         # import here, cause outside the eggs aren't loaded
-#         import pytest
-#         errno = pytest.main(self.pytest_args)
-#         sys.exit(errno)
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 VERSION = '0.0.1'
 
@@ -30,7 +30,7 @@ setup(
     license = "BSD",
     url = "https://github.com/alvinwan/TexSoup",
     packages = ['TexSoup'],
-    # cmdclass = {'test': PyTest},
+    cmdclass = {'test': PyTest},
     tests_require = ['pytest'],
     install_requires = [],
     download_url = 'https://github.com/alvinwan/TexSoup/archive/%s.zip' % VERSION,
