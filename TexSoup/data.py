e017b15d4ea70a7078c1c8e14a7f2faa808b8b82
Alvin Wan <hi@alvinwan.com>
2016-05-19 22:13:21 -0700

    do not render \begin \end for doc

--- TexSoup/data.py
+++ TexSoup/data.py
@@ -204,8 +204,11 @@ class TexEnv(TexExpr):
                 yield content
 
     def __str__(self):
+        contents = '\n'.join(map(str, self._contents))'
+        if self.name == '[tex]':
+            return contents
         return '\\begin{%s}%s\n%s\n\\end{%s}' % (
-            self.name, self.args,'\n'.join(map(str, self._contents)), self.name)
+            self.name, self.args, contents, self.name)
 
     def __repr__(self):
         if not self.args:
